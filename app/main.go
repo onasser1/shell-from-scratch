8b625b2e94dfa5ad40c649c270a8df9d16d76584
Omar Nasser <omarnasserjr@gmail.com>
2025-12-13 07:52:09 +0200

    feat(cli): Implement basic echo function for shell
    Signed-off-by: Omar Nasser <omarnasserjr@gmail.com>

--- app/main.go
+++ app/main.go
@@ -10,17 +10,39 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
+func echoFunc(cmdList []string) {
+	if len(cmdList) == 1 {
+		fmt.Println()
+		return
+	}
+	for i := 1; i < len(cmdList)-1; i++ {
+		fmt.Printf("%s ", cmdList[i])
+	}
+	fmt.Print(cmdList[len(cmdList)-1])
+}
+
 func mainLoop() {
 	fmt.Print("$ ")
 	cmdInput, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
 	if err != nil {
 		fmt.Println("error reading command input")
 	}
-	cmd := strings.TrimSuffix(cmdInput, "\n")
-	if cmd == "exit" {
-		os.Exit(127)
+	cmdList := strings.Split(cmdInput, " ")
+	if len(cmdList) == 0 {
+		return
 	}
-	fmt.Printf("%s: command not found\n", cmd)
+
+	trimmedCommand := strings.TrimSuffix(cmdList[0], "\n")
+	switch trimmedCommand {
+	case "exit":
+		os.Exit(127)
+	case "echo":
+		echoFunc(cmdList)
+	default:
+		fmt.Printf("%s: command not found\n", trimmedCommand)
+	}
+
 }
 
 func main() {
