e772ff5a164ac039d3a8ad391f4fe9ee59ee6ff5
Omar Nasser <omarnasserjr@gmail.com>
2025-12-14 08:31:50 +0200

    feat(cli): modify path argument to read systen environemnt variable instead of static value
    Signed-off-by: Omar Nasser <omarnasserjr@gmail.com>

--- app/main.go
+++ app/main.go
@@ -8,7 +8,6 @@ import (
 )
 
 const (
-	PATH              = "/usr/local/bin:/usr/bin:/usr/sbin/:/bin"
 	PathListSeparator = ":"
 )
 
@@ -41,6 +40,7 @@ func typeFunc(cmdList []string) {
 }
 
 func LookForDirectories(tCmd string) {
+	PATH := os.Getenv("PATH")
 	directores := strings.Split(PATH, PathListSeparator)
 	ReadDirs(directores, tCmd)
 }
